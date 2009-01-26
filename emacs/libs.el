1ad26a3876c9ab807018cd35eb867efc35645479
doran <doran@tineee.(none)>
2009-01-25 22:02:25 -0600

    Added initial java support

---
+++ emacs/libs.el
@@ -0,0 +1,6 @@
+; Load cedet
+(add-to-list 'load-path "~/elisp/emacs/cedet/common")
+(load-file "~/elisp/emacs/cedet/common/cedet.el")
+
+; Load elib
+(add-to-list 'load-path "~/elisp/emacs/elib")
---
+++ java/emacs.java.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/elisp/java/jde/lisp")
+
+(require 'jde)
